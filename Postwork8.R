49439d3e762509c18dc4a07dcaf34aff4235a73d
FausDav <fauspuga@gmail.com>
2022-12-18 16:20:22 -0600

    Versión final Postwork8.R

--- Postwork8.R
+++ Postwork8.R
@@ -10,11 +10,20 @@
 
 # - Haber realizado los works y postworks previos 
 # - Tener una cuenta en Github o en RStudioCloud
+install.packages("dplyr")
+install.packages("ggplot")
+install.packages("DescTools")
+install.packages("moments")
+
 library(dplyr)
 library(ggplot2)
 library(DescTools)
 library(moments)
+
 #### DESARROLLO
+
+
+# 1) Plantea el problema del caso
 
 # Un centro de salud nutricional está interesado en analizar estadísticamente y probabilísticamente los patrones de gasto en alimentos saludables y no saludables en los hogares mexicanos con base en su nivel socioeconómico, en si el hogar tiene recursos financieros extrar al ingreso y en si presenta o no inseguridad alimentaria. Además, está interesado en un modelo que le permita identificar los determinantes socioeconómicos de la inseguridad alimentaria.
 
@@ -32,14 +41,11 @@ library(moments)
 # - ln_alns (Logarítmo natural del gasto en alimentos no saludables)
 # - IA (Inseguridad alimentaria en el hogar): 0 "No presenta IA", 1 "Presenta IA"
 
+# 2) Realiza un análisis descriptivo de la información
 df <- read.csv("./Data/inseguridad_alimentaria_bedu.csv")
 
-
-# 1) Plantea el problema del caso
-"Inserte plateamiento aquí"
-
-# 2) Realiza un análisis descriptivo de la información
 str(df)
+
 "En primera instancia se observa que los datos contienen varios campos categóricos, por lo que se empieza convirtiendo las variables 'area', 'refin','sexojef' e 'IA' en variables cualitativas y a la variable 'nse5f' en variable cualitativa ordinal."
 
 df <- df %>% mutate(nse5f = factor(nse5f, levels = c(1,2,3,4,5),labels = c("Bajo","Medio bajo","Medio","Medio alto","Alto"),ordered = T),
@@ -49,97 +55,93 @@ df <- df %>% mutate(nse5f = factor(nse5f, levels = c(1,2,3,4,5),labels = c("Bajo
                     IA = factor(IA, labels = c('No presenta IA','Presenta IA')))
 str(df)
 
-df.complete.cases <- na.omit(df)
 df.clean <- na.omit(df)
-attach(df.complete.cases)
-
-dim(df)
-dim(df.complete.cases)
+attach(df.clean)
 
 par(mfrow=c(2,3))
 
-plot(df.complete.cases$nse5f) # <-
+plot(nse5f, main = "Nivel socioeconómico") # <-
 
-plot(df.complete.cases$refin) # <-
+plot(refin, main = "Recursos financieros distintos al laboral") # <-
 
-hist(df.complete.cases$ln_als) # <-
-mean(df.complete.cases$ln_als)
-median(df.complete.cases$ln_als)
-Mode(df.complete.cases$ln_als)[1]
-sd(df.complete.cases$ln_als)
-Quantile(df.complete.cases$ln_als, probs = seq(0,1,0.25))
-
-hist(df.complete.cases$ln_alns) # <-
-mean(df.complete.cases$ln_alns)
-median(df.complete.cases$ln_alns)
-Mode(df.complete.cases$ln_alns)[1]
-sd(df.complete.cases$ln_alns)
-Quantile(df.complete.cases$ln_alns, probs = seq(0,1,0.25))
-
-plot(df.complete.cases$IA) # <-
-
+hist(ln_als, main = "Gasto en alimentos saludables") # <-
+mean(ln_als)
+median(ln_als)
+Mode(ln_als)[1]
+sd(ln_als)
+Quantile(ln_als, probs = seq(0,1,0.25))
 skewness(ln_als) #Sesgo a la izquierda
 kurtosis(ln_als) #Leptocúrtica
+
+hist(df.clean$ln_alns, main = "Gasto en alimentos no saludables") # <-
+mean(df.clean$ln_alns)
+median(df.clean$ln_alns)
+Mode(df.clean$ln_alns)[1]
+sd(df.clean$ln_alns)
+Quantile(df.clean$ln_alns, probs = seq(0,1,0.25))
 skewness(ln_alns) #Sesgo a la derecha
 kurtosis(ln_alns) #Platocúrtica
 
+plot(df.clean$IA, main = "Inseguridad alimentaria") # <-
 
-g <- ggplot(data = df.complete.cases, aes(x = nse5f, y = ln_als, col = IA)) +
+dev.off()
+
+g <- ggplot(data = df.clean, aes(x = nse5f, y = ln_als, col = IA)) +
   geom_boxplot() +
-  labs(title = 'Boxplot por Gasto/IA', x = 'Nivel Socieconómico', y = '') +
+  labs(title = 'Boxplot por Gasto en alimentos saludables/IA', x = 'Nivel Socieconómico', y = 'Gasto(ln)') +
   theme_minimal()
 g
 
-g <- ggplot(data = df.complete.cases, aes(x = nse5f, y = ln_alns, col = IA)) +
+g <- ggplot(data = df.clean, aes(x = nse5f, y = ln_alns, col = IA)) +
   geom_boxplot() +
-  labs(title = 'Boxplot por Gasto/IA', x = 'Nivel Socieconómico', y = '') +
+  labs(title = 'Boxplot por Gasto en alimentos no saludables/IA', x = 'Nivel Socieconómico', y = 'Gasto(ln)') +
   theme_minimal()
 g
 
 # 3) Calcula probabilidades que nos permitan entender el problema en México
 "Probabilidad de Presentar IA"
-probabilidad.IA=sum(df.clean$IA)/count(df.clean)
+probabilidad.IA=nrow(df.clean[df.clean$IA == "Presenta IA",])/nrow(df.clean)
 probabilidad.IA
 
 "Probabilidad de Tener Recursos financieros distintos al ingreso laboral"
-probabilidad.refin=sum(df.clean$refin)/count(df.clean)
+probabilidad.refin=nrow(df.clean[df.clean$refin == "si",])/nrow(df.clean)
 probabilidad.refin
 
-"Probabilidades de presenta IA según el nivel socieconómico"
-probabilidad.IA.dado.nivelbajo=sum(df.clean$IA[df.clean$nse5f==1] )/count(df.clean)
+"Probabilidades de presenta IA según el nivel socieconómico contra el total de la muestra"
+{
+  probabilidad.IA.dado.nivelbajo=nrow(df.clean[df.clean$nse5f=="Bajo" & df.clean$IA == "Presenta IA",] )/nrow(df.clean);
+  probabilidad.IA.dado.nivelmediobajo=nrow(df.clean[df.clean$nse5f=="Medio bajo" & df.clean$IA == "Presenta IA",] )/nrow(df.clean);
+  probabilidad.IA.dado.nivelmedio=nrow(df.clean[df.clean$nse5f=="Medio" & df.clean$IA == "Presenta IA",] )/nrow(df.clean);
+  probabilidad.IA.dado.nivelmedioalto=nrow(df.clean[df.clean$nse5f=="Medio alto" & df.clean$IA == "Presenta IA",] )/nrow(df.clean);
+  probabilidad.IA.dado.nivelalto=nrow(df.clean[df.clean$nse5f=="Alto" & df.clean$IA == "Presenta IA",] )/nrow(df.clean);
+}
+
+"Probabilidades de presenta IA según el nivel socieconómico contra el total de la muestra"
+probabilidad.IA.dado.nivelbajo;
+probabilidad.IA.dado.nivelmediobajo;
+probabilidad.IA.dado.nivelmedio;
+probabilidad.IA.dado.nivelmedioalto;
+probabilidad.IA.dado.nivelalto;
+
+  
+"Probabilidades de presenta IA según el nivel socieconómico contra el mismo NSE"
+{
+  probabilidad.IA.dado.nivelbajo=nrow(df.clean[df.clean$nse5f=="Bajo" & df.clean$IA == "Presenta IA",] )/nrow(df.clean[df.clean$nse5f=="Bajo",])
+  probabilidad.IA.dado.nivelmediobajo=nrow(df.clean[df.clean$nse5f=="Medio bajo" & df.clean$IA == "Presenta IA",] )/nrow(df.clean[df.clean$nse5f=="Medio bajo",])
+  probabilidad.IA.dado.nivelmedio=nrow(df.clean[df.clean$nse5f=="Medio" & df.clean$IA == "Presenta IA",] )/nrow(df.clean[df.clean$nse5f=="Medio",])
+  probabilidad.IA.dado.nivelmedioalto=nrow(df.clean[df.clean$nse5f=="Medio alto" & df.clean$IA == "Presenta IA",] )/nrow(df.clean[df.clean$nse5f=="Medio alto",])
+  probabilidad.IA.dado.nivelalto=nrow(df.clean[df.clean$nse5f=="Alto" & df.clean$IA == "Presenta IA",] )/nrow(df.clean[df.clean$nse5f=="Alto",])
+}
+"Probabilidades de presenta IA según el nivel socieconómico contra el mismo NSE"
 probabilidad.IA.dado.nivelbajo
-
-probabilidad.IA.dado.nivelmediobajo=sum(df.clean$IA[df.clean$nse5f==2] )/count(df.clean)
 probabilidad.IA.dado.nivelmediobajo
-
-probabilidad.IA.dado.nivelmedio=sum(df.clean$IA[df.clean$nse5f==3] )/count(df.clean)
 probabilidad.IA.dado.nivelmedio
-
-probabilidad.IA.dado.nivelmedioalto=sum(df.clean$IA[df.clean$nse5f==4] )/count(df.clean)
 probabilidad.IA.dado.nivelmedioalto
-
-probabilidad.IA.dado.nivelalto=sum(df.clean$IA[df.clean$nse5f==5] )/count(df.clean)
 probabilidad.IA.dado.nivelalto
-
-"Probabilidades de presenta IA según el nivel socieconómico"
-probabilidad.IA.dado.nivelbajo=sum(df.clean$IA[df.clean$nse5f==1] )/count(df.clean[df.clean$nse5f==1,])
-probabilidad.IA.dado.nivelbajo
-
-probabilidad.IA.dado.nivelmediobajo=sum(df.clean$IA[df.clean$nse5f==2] )/count(df.clean[df.clean$nse5f==2,])
-probabilidad.IA.dado.nivelmediobajo
-
-probabilidad.IA.dado.nivelmedio=sum(df.clean$IA[df.clean$nse5f==3] )/count(df.clean[df.clean$nse5f==3,])
-probabilidad.IA.dado.nivelmedio
-
-probabilidad.IA.dado.nivelmedioalto=sum(df.clean$IA[df.clean$nse5f==4] )/count(df.clean[df.clean$nse5f==4,])
-probabilidad.IA.dado.nivelmedioalto
-
-probabilidad.IA.dado.nivelalto=sum(df.clean$IA[df.clean$nse5f==5] )/count(df.clean[df.clean$nse5f==5,])
-probabilidad.IA.dado.nivelalto
+  
 # 4) Plantea hipótesis estadísticas y concluye sobre ellas para entender el problema en México
 
 #########################################################
-"Plantea hipótesis estadísticas y concluye sobre ellas para entender el problema en México"
 " La mayoría de las personas afirman que los hogares con menor nivel socioeconómico tienden 
 a gastar más en productos no saludables que las personas con mayores niveles socioeconómicos 
 y que esto, entre otros determinantes, lleva a que un hogar presente cierta inseguridad alimentaria."
@@ -148,16 +150,18 @@ y que esto, entre otros determinantes, lleva a que un hogar presente cierta inse
 Ha: promedio de gasto de productos no saludable del nivel socio economico bajo y medio bajo > promedio de gasto no saludable del nivel medio, medio alto y alto"
 
 "Primero hay que checar las varianzas de los grupos de nivel socio economico bajo(bajo y medio bajo) y alto(medio, medio alto y alto) si son iguales o diferentes
-Ho: varianzas"
-var.test(df.clean$ln_alns[df.clean$nse5f=="Bajo" | df.clean$nse5f == "Medio bajo"], 
-         df.clean$ln_alns[df.clean$nse5f=="Medio" | df.clean$nse5f == "Medio alto" | df.clean$nse5f == "Alto"], 
+Ho: varianzas son iguales
+Ha: varianzas son diferentes"
+df.clean
+var.test(df.clean$ln_alns[df.clean$nse5f=="Bajo" | df.clean$nse5f == "Medio bajo" | df.clean$nse5f == "Medio"], 
+         df.clean$ln_alns[df.clean$nse5f == "Medio alto" | df.clean$nse5f == "Alto"], 
          ratio = 1, alternative = "two.sided")
 "Con un p-value=2.2e-16 < 0.05, se concluye que se rechaz Ho. Entonces, las varianzas son diferentes."
 
-t.test(x = df.clean$ln_alns[df.clean$nse5f=="Bajo" | df.clean$nse5f == "Medio bajo"], y = df.clean$ln_alns[df.clean$nse5f=="Medio" | df.clean$nse5f == "Medio alto" | df.clean$nse5f == "Alto"],
+t.test(x = df.clean$ln_alns[df.clean$nse5f=="Bajo" | df.clean$nse5f == "Medio bajo" | df.clean$nse5f == "Medio"], y = df.clean$ln_alns[df.clean$nse5f == "Medio alto" | df.clean$nse5f == "Alto"],
        alternative = "greater",
        mu = 0, var.equal = FALSE)
-"Con un p-value = 1 > 0.05, no hay evidencia estadistica para rechazas Ho. Entonces, el gasto promedio de productos no saludables del nivel socioeconomico bajo(bajo y medio bajo) es menor o igual que los del nivel socioeconomico alto(medio, medio alto, y alto)."
+"Con un p-value = 1 > 0.05, no hay evidencia estadistica para rechazas Ho. Entonces, el gasto promedio de productos no saludables del nivel socioeconomico bajo(bajo, medio bajo y medio) es menor o igual que los del nivel socioeconomico alto(medio alto, y alto)."
 
 #Hipotesis 2
 "
@@ -165,23 +169,40 @@ Ho: Inseguridad alimentaria nivel socioeconomico bajo <= Inseguridad alimentaria
 Ha: Inseguridad alimentaria nivel socioeconomico bajo > Inseguridad alimentaria nivel socioeconomico alto
 "
 
-var.test(df.clean$IA[df.clean$nse5f==1 | df.clean$nse5f == 2], 
-         df.clean$IA[df.clean$nse5f== 3| df.clean$nse5f == 4 | df.clean$nse5f == 5], 
+#probabilidad.IA=length(df.clean$IA[df.clean$IA=="Presenta IA"])/count(df.clean)
+
+{sampleBajo <- c()
+  for (i in 1:30) {
+    sample.test <- df[sample(1:dim(df)[1], size = 1000),c("IA","nse5f")]
+    # sample40[i] <- nrow(sample.test[sample.test$nse5f=="Bajo",])/nrow(sample.test)
+    sampleBajo[i] <- nrow(sample.test[sample.test$IA=="Presenta IA" & 
+                                        (sample.test$nse5f=="Bajo" |
+                                           sample.test$nse5f=="Medio bajo" |
+                                           sample.test$nse5f=="Medio"),])/nrow(sample.test)
+  }
+  hist(sampleBajo, main = "Muestra Bajo", xlab = "")
+}
+
+{sampleAlto <- c()
+  for (i in 1:30) {
+    sample.test <- df[sample(1:dim(df)[1], size = 1000),c("IA","nse5f")]
+    # sample40[i] <- nrow(sample.test[sample.test$nse5f=="Bajo",])/nrow(sample.test)
+    sampleAlto[i] <- nrow(sample.test[sample.test$IA=="Presenta IA" & 
+                                        (sample.test$nse5f=="Medio alto" |
+                                        sample.test$nse5f=="Alto")
+                                      ,])/nrow(sample.test)
+  }
+  hist(sampleAlto, main = "Muestra Alto", xlab = "")
+}
+
+var.test(sampleBajo,
+         sampleAlto,
          ratio = 1, alternative = "two.sided")
-"Con un p-value=2.2e-16 < 0.05, se concluye que se rechaz Ho. Entonces, las varianzas son diferentes."
+"Con un p-value=0.3874 > 0.05, se concluye que no se rechaza Ho. Entonces, las varianzas son iguales."
 
-t.test(x = df.clean$IA[df.clean$nse5f==1 | df.clean$nse5f == 2], y = df.clean$IA[df.clean$nse5f== 3| df.clean$nse5f == 4 | df.clean$nse5f == 5],
+t.test(x = sampleBajo, y = sampleAlto,
        alternative = "greater",
-       mu = 0, var.equal = FALSE)
-
-# var.test(df.clean$IA[df.clean$nse5f=="Bajo" | df.clean$nse5f == "Medio bajo"], 
-#          df.clean$IA[df.clean$nse5f== "Medio"| df.clean$nse5f == "Medio alto" | df.clean$nse5f == "Alto"], 
-#          ratio = 1, alternative = "two.sided")
-# "Con un p-value=2.2e-16 < 0.05, se concluye que se rechaz Ho. Entonces, las varianzas son diferentes."
-# 
-# t.test(x = df.clean$IA[df.clean$nse5f=="Bajo" | df.clean$nse5f == "Medio bajo"], y = df.clean$IA[df.clean$nse5f=="Medio" | df.clean$nse5f == "Medio alto" | df.clean$nse5f == "Alto"],
-#        alternative = "greater",
-#        mu = 0, var.equal = FALSE)
+       mu = 0, var.equal = TRUE)
 
 "Con un p-value=2.2e-16 < 0.05, se rechaza Ho. Entonces la inseguridad alimentaria en el nivel socioeconomico bajo es mayor que el nivel socioeconomico alto"
 
@@ -194,33 +215,42 @@ logistic.1 <- glm(IA ~ ln_als + ln_alns + nse5f + refin,
                   data = df.clean, family = binomial)
 summary(logistic.1)
 
-logistic.2 <- glm(IA ~ ln_alns + nse5f + refin, 
-                  data = df.clean, family = binomial)
-summary(logistic.2)
-
-pseudo_r2.1 <- (logistic.1$null.deviance - logistic.1$deviance)/logistic.1$null.deviance
-pseudo_r2.1
 #la variable del gasto de productos saludables no es relevante para el modelo logistico.
+
 logistic.2 <- glm(IA ~ ln_alns + nse5f + refin, 
                   data = df.clean, family = binomial)
 summary(logistic.2)
-pseudo_r2.2 <- (logistic.2$null.deviance - logistic.2$deviance)/logistic.2$null.deviance
-pseudo_r2.2
 
+# Probamos agregando el resto de variables
 logistic.3 <- glm(IA ~ ln_alns + nse5f + refin + area + numpeho + edadjef + sexojef + añosedu , 
                   data = df.clean, family = binomial)
 summary(logistic.3)
-pseudo_r2.3 <- (logistic.3$null.deviance - logistic.2$deviance)/logistic.2$null.deviance
-pseudo_r2.3
 
-logistic.4 <- glm(IA ~ ln_alns + nse5f + refin  + numpeho + sexojef + añosedu , 
+# Quitamos las variables Área y Edad del jefe de familia por ser menos significativas
+logistic.4 <- glm(IA ~ ln_alns + nse5f + refin + area  + numpeho + sexojef + añosedu , 
                   data = df.clean, family = binomial)
 summary(logistic.4)
-pseudo_r2.4 <- (logistic.4$null.deviance - logistic.2$deviance)/logistic.2$null.deviance
-pseudo_r2.4
-#El logistic.4 tiene todas las variables significativas para el modelo (ln_alns, nse5f, refin, numpeho, sexojef y añosedu)
 
-"Ya que el valor del pseudo r2 para el modelo logístico logistic.2 es el mayor de todos, será dicho modelo con el que quedan definidos los determinantes de la inseguridad alimenticia, siendo estos: Gasto en comida no saludablo, Nivel socieconómico y si la familia cuenta con un ingreso distinto al laboral o no."
+
+pseudo_r2.1 <- (logistic.1$null.deviance - logistic.1$deviance)/logistic.1$null.deviance
+pseudo_r2.2 <- (logistic.2$null.deviance - logistic.2$deviance)/logistic.2$null.deviance
+pseudo_r2.3 <- (logistic.3$null.deviance - logistic.3$deviance)/logistic.3$null.deviance
+pseudo_r2.4 <- (logistic.4$null.deviance - logistic.4$deviance)/logistic.4$null.deviance
+
+"Se evelúan los valores pseudo R2"
+pseudo_r2.1
+pseudo_r2.2
+pseudo_r2.3
+pseudo_r2.4
+
+"Se evaluán los Criterio de información de Akaike"
+AIC(logistic.1)
+AIC(logistic.2)
+AIC(logistic.3)
+AIC(logistic.4)
+
+#El logistic.3 tiene todas las variables significativas para el modelo (ln_alns, nse5f, refin, numpeho, sexojef y añosedu)
+# Sin embargo decidimos tomar el modelo logistic.4 ya que explica prácticamente con la misma precisión y tiene menos variables.
 
 # 6) Escribe tu análisis en un archivo README.MD y tu código en un script de R y publica ambos en un repositorio de Github.
 
